83367e8e34f7e6b7a3a01bc87af509f2fdaa8cf8
Samuel Graham <sam.graham@solertium.com>
2023-02-20 22:22:48 -0500

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="text-3xl">
+        ChatDeck coming soon!
+      </div>
+    </div>
+  )
+}
+
+export default App;
