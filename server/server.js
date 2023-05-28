91755f5fe6374e667e1ed618b8b899d58d3a3c78
Samuel Graham <sam.graham@solertium.com>
2023-05-28 10:18:45 -0400

    database connection

--- server/server.js
+++ server/server.js
@@ -2,12 +2,43 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
+require ('dotenv').config();
+console.log(process.env)
+const { Pool }  = require ('pg');
+
+const {
+POSTGRES_HOST,
+POSTGRES_DB,
+POSTGRES_USER,
+POSTGRES_PASSWORD
+} = process.env
+
+const client = new Pool({
+    host: POSTGRES_HOST,
+    database: POSTGRES_DB,
+    user: POSTGRES_USER,
+    password: POSTGRES_PASSWORD
+});
+
 const port = process.env.PORT || 3000;
 const { hashPassword } = require('./helpers/hashPassword');
 
 app.use(cors());
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+
+app.get('/', (req, res) => {
+  client.query('SELECT * FROM users', (err, queryResult) => {
+    if (err) {
+      console.error('Error executing query', err.stack);
+      res.status(500).json({ error: 'An error occurred while fetching users' });
+    } else {
+      console.log(queryResult.rows);
+      res.status(200).json(queryResult.rows);
+    }
+  });
+});
+
 
 
 app.post('/signin', (req, res) => {
